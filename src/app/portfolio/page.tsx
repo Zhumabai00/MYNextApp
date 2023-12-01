6a1eb75b40783f2e9af10b7527362e6e28a119e5
user <user@MacBook-Pro.local>
2023-12-01 08:48:37 +0700

    Create work

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -8,37 +8,17 @@ export default function Portfolio() {
 		<div className={''}>
 			<h1 className={'my-5'}>Choose a gallery</h1>
 			<div className={'flex gap-[50px]'}>
-				<Link href="/portfolio/illustrations" className={`w-[300px] h-[400px] rounded-[5px] relative border-[#bbb] border-[5px]`}>
-					<Tilt
-						className="w-full h-full bg-pattern bg-center bg-cover item rounded-[5px]"
-						options={{
-							scale: 1,
-						}}
-					>
-						{/* <div className=""> */}
-						<span className={'absolute right-[10px] bottom-[10px] text-4xl font-bold title'}>Illustrations</span>
-						{/* </div> */}
-					</Tilt>
+				<Link href="/portfolio/illustrations" className={`bg-pattern bg-center bg-cover item rounded-[5px] w-[300px] h-[400px] rounded-[5px] relative border-[#bbb] border-[5px]`}>
+
+					{/* <div className=""> */}
+					<span className={'absolute right-[10px] bottom-[10px] text-4xl font-bold title'}>Illustrations</span>
+					{/* </div> */}
 				</Link>
-				<Link href="/portfolio/websites" className={'rounded-[5px] w-[300px] h-[400px] relative border-[#bbb] border-[5px]'}>
-					<Tilt
-						className="w-full h-full bg-center bg-cover item2 rounded-[5px]"
-						options={{
-							scale: 1,
-						}}
-					>
-						<span className={'absolute right-[10px] bottom-[10px] text-4xl font-bold title'}>Websites</span>
-					</Tilt>
+				<Link href="/portfolio/websites" className={'bg-center bg-cover item2 rounded-[5px] rounded-[5px] w-[300px] h-[400px] relative border-[#bbb] border-[5px]'}>
+					<span className={'absolute right-[10px] bottom-[10px] text-4xl font-bold title'}>Websites</span>
 				</Link>
-				<Link href="/portfolio/application" className={'rounded-[5px] w-[300px] h-[400px] relative border-[#bbb] border-[5px]'}>
-					<Tilt
-						options={{
-							scale: 1,
-						}}
-						className="w-full h-full bg-center bg-cover item3 rounded-[5px]"
-					>
-						<span className={'absolute right-[10px] bottom-[10px] text-4xl font-bold title'}>Application</span>
-					</Tilt>
+				<Link href="/portfolio/application" className={'bg-center bg-cover item3 rounded-[5px] rounded-[5px] w-[300px] h-[400px] relative border-[#bbb] border-[5px]'}>
+					<span className={'absolute right-[10px] bottom-[10px] text-4xl font-bold title'}>Application</span>
 				</Link>
 			</div>
 		</div>
